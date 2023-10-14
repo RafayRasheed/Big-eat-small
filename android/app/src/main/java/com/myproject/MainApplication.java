271daab5643ec681360c58d46b240e3b0d32d877
Rafay Rasheed <rafayrasheed777.rr@gmail.com>
2023-10-15 00:11:23 +0500

    foo4yeed

--- android/app/src/main/java/com/myproject/MainApplication.java
+++ android/app/src/main/java/com/myproject/MainApplication.java
@@ -1,4 +1,4 @@
-package com.myfoodapp;
+package com.bigeatsmall;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- android/app/src/main/java/com/myproject/SplashActivity.java
+++ android/app/src/main/java/com/myproject/SplashActivity.java
@@ -1,4 +1,4 @@
-package com.myfoodapp;
+package com.bigeatsmall;
 
 import android.content.Intent;
 import android.os.Bundle;
