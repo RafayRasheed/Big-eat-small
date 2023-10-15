af835edb262d1c42066c1373fd579d6d5d94836e
Rafay Rasheed <rafayrasheed777.rr@gmail.com>
2023-10-15 22:03:28 +0500

    dhdh

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { View, Text, SafeAreaView, StatusBar, Platform, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, SafeAreaView, StatusBar, Platform, TouchableOpacity, StyleSheet, Alert } from 'react-native';
 // import { MMKV } from 'react-native-mmkv';
 import { myColors } from './ultils/myColors';
 import { myHeight, printWithPlat } from './game/common';
@@ -11,6 +11,8 @@ import storeRedux from './redux/store_redux';
 import { setMute } from './redux/states_reducer';
 import { getMuteStorage } from './game/storageFun';
 import KeepAwake from 'react-native-keep-awake';
+import database from '@react-native-firebase/database';
+import uuid from 'react-native-uuid';
 
 // import { enableLatestRenderer } from 'react-native-maps';
 
@@ -20,17 +22,95 @@ import KeepAwake from 'react-native-keep-awake';
 
 
 export default function App() {
+  function Yes() {
 
+    database()
+      .ref(`/game/playing`)
+      .orderByKey()
+      .once('value')
+      .then(snapshot => {
+        let s = 0
+        let found = null
+        let deleted = []
+        snapshot.forEach(documentSnapshot1 => {
+          const order = documentSnapshot1.val()
+          if (order.active) {
+            s += 2
+          } else {
+            s += 1
+          }
+          if (found) {
+            return
+          }
+
+          if (!order.player1) {
+            deleted.push(documentSnapshot1.key)
+            return
+          }
+          if (!order.player2) {
+            found = documentSnapshot1.key
+            return
+          }
+
+        });
+        console.log('Players Online', s)
+        console.log('Player Game', found)
+        console.log('Deleted Game', deleted)
+
+
+      })
+      .catch((er) => {
+        console.log('Error on set order',)
+
+      });
+
+  }
+  function getDateInt(date) {
+    const adjustSting = function (string, size) {
+      const len = string.length
+      let myStr = ''
+      for (let i = 0; i < size - len; i++) {
+        myStr += '0'
+      }
+      return (myStr + string)
+
+    }
+
+    const year = adjustSting(date.getUTCFullYear().toString(), 2)
+    const month = adjustSting(date.getUTCMonth().toString(), 2)
+    const day = adjustSting(date.getUTCDate().toString(), 2)
+    const hours = adjustSting(date.getUTCHours().toString(), 2)
+    const minutes = adjustSting(date.getUTCMinutes().toString(), 2)
+    const seconds = adjustSting(date.getUTCSeconds().toString(), 2)
+    const mili = adjustSting(date.getUTCMilliseconds().toString(), 3)
+    const code = year + month + day + hours + minutes + seconds + mili
+
+    return code
+
+
+  }
   useEffect(() => {
     printWithPlat('Started Successfully')
     SplashScreen.hide()
     KeepAwake.activate();
+    // console.log(getDateInt(new Date()))
+    Yes()
 
-    // dispatch(setMute(getMuteStorage()))
-    // const dispatch = useDispatch()
-    // dispatch(setCart(getCartLocal()))
-    // console.log(typeof getCartLocal())
-    // printWithPlat("Is MMKV store successful? " + storage.contains('mberr'))
+
+    //   .ref(`/game/playing`).child(getDateInt(new Date()))
+    //   .set({
+    //     player1: 'rafay6',
+    //     player2: 'rafay7',
+    //     active: true
+    //   }).then(() => {
+    //     // console.log('Han Ustaad Hogaya', d)
+
+    //   })
+    //   .catch((er) => {
+    //     console.log('Error on set order',)
+
+    //   })
+
   }, [])
   const isAndroid = Platform.OS == 'android'
   // const OsVer = Platform.constants['Release']; Android Version like 9,10, 11
