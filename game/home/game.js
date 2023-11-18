446c57733403f783332752ba50d0a3f04a01bc88
RafayRasheed <rafayrasheed777.rr@gmail.com>
2023-11-18 23:04:26 +0500

    ttq5

--- game/home/game.js
+++ game/home/game.js
@@ -119,46 +119,63 @@ export const Game = ({ navigation }) => {
     }
     function BotPlay() {
 
-        let mo2 = []
 
         // mockInLines.map((it, i) => {
         //     const { player, size } = it
         //     mo2.push({ player, size })
         // })
-        let p0Mock = []
-        let p1Mock = []
-        playerZeroMocks.map((p, index) => {
-            if(p.show && p0Mock.findIndex(it => it.size == p.size) == -1){
-                p0Mock.push({...p, index})
-            }
+        // let p0Mock = []
+        // let p1Mock = []
+        // playerZeroMocks.map((p, index) => {
+        //     if(p.show && p0Mock.findIndex(it => it.size == p.size) == -1){
+        //         p0Mock.push({...p, index})
+        //     }
 
-        })
-        playerOneMocks.map((p, index) => {
-            if(p.show && p1Mock.findIndex(it => it.size == p.size) == -1){
-                p1Mock.push({...p, index})
-            }
+        // })
+        // playerOneMocks.map((p, index) => {
+        //     if(p.show && p1Mock.findIndex(it => it.size == p.size) == -1){
+        //         p1Mock.push({...p, index})
+        //     }
 
 
-        })
+        // })
         // const s = minimax2(mo2, 0, -10000, 10000, 0,p0Mock, p1Mock ,p0Mock[p0Mock.length-1].size,p0Mock.length-1 )
-
-        const mySize = 2
-        mockInLines.map((it, i) => {
-            const { player, size } = it
-            if(player!=null&& size<mySize){
-                mo2.push({ player:null, size:null })
-                console.log('g')
-
+       
+       
+       
+       
+        let p0Mock = []
+        playerZeroMocks.map((p, index) => {
+            if(p.show && p0Mock.findIndex(it => it == p.size) == -1){
+                p0Mock.push( p.size)
             }
-            else{
 
-                mo2.push({ player, size })
-            }
         })
-        console.log('------------------------------------------',mo2)
-        console.log('------------------------------------------')
-        const s = minimaxEasy(mo2, 0, -10000, 10000, 0,)
+        const result = []
 
+       console.log(p0Mock) 
+       p0Mock.map((mySize)=>{
+        let mo2 = []
+
+           mockInLines.map((it, i) => {
+               const { player, size } = it
+               if(player!=null&& size<mySize){
+                   mo2.push({ player:null, size:null })
+    
+               }
+               else{
+    
+                   mo2.push({ player, size })
+               }
+           })
+           console.log('------------------------------------------')
+           console.log('------------------------------------------')
+           const s = minimaxEasy(mo2, 0, -10000, 10000, 0,)
+           result.push(s)
+       })
+
+       console.log(result)
+       return
         goPlayBot(s)
 
     }
